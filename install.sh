5980508ea8b524c74f34c2b50d08362fafffd405
adam-coates <adamcoates175@gmail.com>
2024-10-25 18:25:48 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+yaypackages=(
+    "hyprland-git"
+    "aylurs-gtk-shell"
+    "matagen-bin"
+)
+
+pacmanpackages=(
+    "hyprland-git"
+    "hyprpaper"
+    "chromium"
+    "nwg-look"
+    "ttf-font-awesome"
+    "neovim"
+    "kitty"
+    "dart-sass"
+    "fd"
+    "brightnessctl"
+    "swww"
+    "hyprpicker"
+)
+
+sudo pacman -S --noconfirm git 
+
+sudo pacman -S --noconfirm gum 
+
+
+
+
+
